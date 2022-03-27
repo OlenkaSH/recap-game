8129593bd538e44de593074ec523b70b643ff7f9
Olena Kraemer <olenakraem@gmail.com>
2022-03-27 13:08:47 +0200

    first trying to solve

---
+++ Module3/labs/lab-hello-ironhack/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const statement = <h1> React is cool! </h1>;
+
+function App() {
+  return <div className="App">{statement}</div>;
+}
+
+export default App;
